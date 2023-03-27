24ee2ab24746d8f948019232177f7f8bf3c5dfd2
Juned KH <kjuned007@gmail.com>
2023-03-27 11:15:54 +0530

    Minor Fix

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -291,18 +291,24 @@ def antfiles(url: str) -> str:
     Based on https://github.com/zevtyardt/lk21
     """
     try:
-        return Bypass().bypass_antfiles(url)
+        link = Bypass().bypass_antfiles(url)
     except Exception as e:
         raise DirectDownloadLinkException(f"ERROR: {e.__class__.__name__}")
+    if not link:
+        raise DirectDownloadLinkException("ERROR: Download link not found")
+    return link
 
 def streamtape(url: str) -> str:
     """ Streamtape direct link generator
     Based on https://github.com/zevtyardt/lk21
     """
     try:
-        return Bypass().bypass_streamtape(url)
+        link = Bypass().bypass_streamtape(url)
     except Exception as e:
         raise DirectDownloadLinkException(f"ERROR: {e.__class__.__name__}")
+    if not link:
+        raise DirectDownloadLinkException("ERROR: Download link not found")
+    return link
 
 def racaty(url: str) -> str:
     """ Racaty direct link generator
